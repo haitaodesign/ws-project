f0386cc2cd5c1a639821380a82670d2f6d8f44f2
tangxianju <1597113017@qq.com>
2017-10-10 17:05:14 +0800

    查询

--- src/views/project/CountProject/index.vue
+++ src/views/project/CountProject/index.vue
@@ -2,12 +2,12 @@
   <div>
       <PageTitle :BreadData="breadData"></PageTitle>
       <div class="apply-baseinfo clearfix">
-         <Form inline :label-width="90" :model='partInData'>
+         <Form inline :label-width="90" :model='params'>
               <FormItem label="项目发起部门">
-                     <Cascader :data="deptData" v-model="partInData.SquadName"></Cascader>
+                     <Cascader :data="deptData" :load-data="loadGroupData" @on-change="deptChange"></Cascader>
                 </FormItem>
                 <FormItem label="项目发起人">
-                    <Select v-model="partInData.selectman" placeholder="请选择"  style="width:200px">
+                    <Select v-model="params.creater" placeholder="请选择"  style="width:200px">
                         <Option value="beijing">北京市</Option>
                         <Option value="shanghai">上海市</Option>
                         <Option value="shenzhen">深圳市</Option>
@@ -16,40 +16,50 @@
                 <FormItem label="项目发起时间">
                   <Row>
                       <Col span="11">
-                          <DatePicker type="date" placeholder="选择日期" v-model="partInData.dateTime"></DatePicker>
+                          <DatePicker type="date" placeholder="选择日期" v-model="params.createdate1"></DatePicker>
                       </Col>
                       <Col span="2" style="text-align: center">-</Col>
                       <Col span="11">
-                          <DatePicker type="date" placeholder="选择日期" v-model="partInData.dateTime2"></DatePicker>
+                          <DatePicker type="date" placeholder="选择日期" v-model="params.createdate2"></DatePicker>
                       </Col>
                   </Row>
               </FormItem>
               <FormItem label="项目完成时间">
                   <Row>
                       <Col span="11">
-                          <DatePicker type="date" placeholder="选择日期" v-model="partInData.dateTime3"></DatePicker>
+                          <DatePicker type="date" placeholder="选择日期" v-model="params.finishdate1"></DatePicker>
                       </Col>
                       <Col span="2" style="text-align: center">-</Col>
                       <Col span="11">
-                          <DatePicker type="date" placeholder="选择日期" v-model="partInData.dateTime4"></DatePicker>
+                          <DatePicker type="date" placeholder="选择日期" v-model="params.finishdate2"></DatePicker>
                       </Col>
                   </Row>
               </FormItem>
               <FormItem label="上线时间">
                   <Row>
                       <Col span="11">
-                          <DatePicker type="date" placeholder="选择日期" v-model="partInData.dateTime"></DatePicker>
+                          <DatePicker type="date" placeholder="选择日期" v-model="params.onlinedate1"></DatePicker>
                       </Col>
                       <Col span="2" style="text-align: center">-</Col>
                       <Col span="11">
-                          <DatePicker type="date" placeholder="选择日期" v-model="partInData.dateTime"></DatePicker>
+                          <DatePicker type="date" placeholder="选择日期" v-model="params.onlinedate2"></DatePicker>
                       </Col>
                   </Row>
               </FormItem>
+              <FormItem label="项目类型">
+                <Select placeholder="请选择"  style="width:200px" v-model="params.proType">
+                    <Option value="1">立项待审批</Option>
+                    <Option value="2">开发中</Option>
+                    <Option value="3">上线待审批</Option>
+                    <Option value="4">完成</Option>
+                    <Option value="5">驳回</Option>
+                    <Option value="6">作废</Option>
+                </Select>
+            </FormItem>
               <FormItem label="">
-                  <Input v-model="partInData.keys" placeholder="请输入关键字"></Input>
+                  <Input v-model="params.param" placeholder="请输入关键字"></Input>
               </FormItem>
-              <Button type="primary" style='margin-left: 30px;'>查询</Button>
+              <Button type="primary" style='margin-left: 30px;' @click="searchForm">查询</Button>
             </Form>
       </div>  
       <div>
@@ -62,6 +72,7 @@ import PageTitle from '../../components/PageTitle'
 import {getselectOnPro} from '../../../api/requestdata'
 import {getDeptData} from '../../../api/requestdata';
 import {getGroupData} from '../../../api/requestdata';
+import {getMembersBySquadId} from '../../../api/requestdata'
 export default {
   components:{
     PageTitle,
@@ -71,16 +82,19 @@ export default {
       breadData:[{
         name:'项目统计',
       }],
-      partInData:{
-           keys:'',
-           selectType:'',
-           dateTime:'',
-           dateTime2:'',
-           dateTime3:'',
-           dateTime4:'',
-           selectman:'',
-           selectpt:'',
-           SquadName:[]
+
+      params:{
+        current:1,
+        pageSize:10,
+        createrSquadid:'',
+        creater:'',
+        createdate1:'',
+        createdate2:'',
+        finishdate1:'',
+        finishdate2:'',
+        onlinedate1:'',
+        onlinedate2:'',
+        param:''
       },
    
       deptData:[],
@@ -100,68 +114,83 @@ export default {
          key:"sum2"
        }
       ],
-      statistics:[
-      ],
+      statistics:[],
+      createrData:[],
+      
       }
    },
   created(){
     this.initDate();
-    this.initDeptData();
-    this.initGroupData()
+   this. initDeptData();
    
     },
   methods:{
     initDate(){
-      getselectOnPro().then(res=>{
+      getselectOnPro(this.params).then(res=>{
         if(res.data.code === 200){
           this.statistics = res.data.data;
           console.log(this.statistics);
         }
       })
     },
-    initGroupData(){
-      getGroupData().then(res=>{
-        let groupdata=res.data.data;
-        console.log( groupdata)
-        // this.linst.push(groupdata)
-        // this.linst.forEach(function(value,index){
-           
-        // },this)
-        console.log(this.linst);
-        groupdata.forEach(function(element) {
-         this.linst.push({
-            value:element.squadid,
-            label:element.squad
-          })
-        }, this);
-        
-      })
-    },
+       // 发起部门数据
     initDeptData(){
       getDeptData().then(res=>{
         let depdata=res.data.data;
-        console.log(depdata)
-        depdata.forEach(function(element,index) {
+        depdata.forEach(function(element) {
           this.deptData.push({
             value:element.departmentid,
             label:element.department,
             children: [],
-           
+            loading: false
           });
-          console.log(this.deptData[index].children)
-        // this.deptData[index].children.push({
-        //     label:this.linst[index].squad
-        // })
-        this.deptData[index].children = this.linst
-       
         },this);
       })
-     
     },
-  
-  
-  
-   }
+    // 组数据
+    initGroupData(params,item){
+      getGroupData(params).then(res=>{
+        let groupdata=res.data.data;
+        groupdata.forEach(function(element) {
+          item.children.push({
+            value:element.squadid,
+            label:element.squad
+          })
+        }, this);
+        item.loading=false;
+      })
+    },
+    // Cascader加载组数据
+    loadGroupData(item,callback){
+      item.loading=true;
+      let deptId=item.value;
+      let params = {
+        id:item.value
+      };
+      this.initGroupData(params,item);
+      callback();
+    },
+    // 监听Cascader组件数据
+    deptChange(value,selectedData){
+      // 通过小组id获取人
+      this.params.createrSquadid = selectedData[1].value;
+      this.initCreaterData();
+    },
+    // 点击查询请求数据
+    searchForm(){
+       this.initDate();
+    },
+    // 初始化项目发起人数据
+    initCreaterData(){
+      const params = {
+        squadId:this.params.createrSquadid
+      }
+      getMembersBySquadId(params).then(res=>{
+          this.createrData = res.data.data; 
+      })
+    },
+
+  }
 }
 </script>
 <style scoped>
